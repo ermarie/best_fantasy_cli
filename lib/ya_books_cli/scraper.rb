c4f784547cbc74cd611daf4120d872fa3d2f2691
Erin Stalzer <erinstalzer@gmail.com>
2019-05-15 17:11:19 +0000

    change code in .scrape_page to new url; add .scrape_book_page

--- lib/ya_books_cli/scraper.rb
+++ lib/ya_books_cli/scraper.rb
@@ -2,29 +2,38 @@ class Scraper
   
   def self.scrape_page
     book_array = []
-    doc = Nokogiri::HTML(open("http://bestfantasybooks.com/best-young-adult-fantasy-books.html"))
-    doc.css("div.pagecontent").each do |book|
-      
-      num = book.css("span.recno").text
-      if num.include?("0") && num.split("")[0] == "0"
-          num = num.split("")[1].to_i
-      end
+    doc = Nokogiri::HTML(open("http://bestfantasybooks.com/lists/list/Crowd/Best-Fantasy-Books-for-Women"))
+    num = 0
+    doc.css("div.list_item").each do |book|
+    num += 1
 
-      book = {
-        :num => num, 
-        :name => book.css("div.pagehd_text").css("h1").text, 
-        :author => book.css("span").css("a").first.text.gsub("(","").gsub(")",""),
-        #:description => get_description
-        :link => book.css("div.carousel_content").css("center").css("a").attribute('href').value
-      }
-      
-      book_array << book 
+    book = {
+    :num => num,
+    :name => book.css("h2").css("a").text,
+    :author => book.css("h3").first.css("a").text,
+    :link => "http://bestfantasybooks.com#{book.css("h2").css("a").attribute('href').value}"
+    }
+
+    book_array << book
     end
-    
+
     Book.create_from_collection(book_array)
   end
   
+  def self.scrape_book_page(book)
+    doc = Nokogiri::HTML(open(book.link))
 
-  
-  
+    book.rating = doc.css("div.item_rating").text
+    book.list_ranking = doc.css("div.item_series").text
+    book.description = doc.css("div.item_description").text
+
+    also_liked_array = []
+    doc.css("div.also_liked_book").each do |also_liked_book|
+
+    book = {
+      :name => also_liked_book.css("div.also_liked_text").css("a").text,
+      :author => also_liked_book.css("div.also_liked_author").text
+     }
+   end
+
 end
